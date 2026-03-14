f06a1c917d72e827b8889237a750bc0f123d99fd
Stanislav <stanislav.igov@casovate.com>
2026-03-14 14:30:39 +0200

    model added

---
+++ backend/src/models/model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    days_free: {
+      type: Number,
+      default: 0
+    },
+    hasLogedForFirstTime: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
+
+const User = mongoose.model('User', userSchema, 'user');
+
+export default User;
