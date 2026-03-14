edae85fbb692285c69b4d3bfb6921f04dd1be0c0
Stanislav <stanislav.igov@casovate.com>
2026-03-14 14:33:15 +0200

    added name to scheama

--- backend/src/models/model.js
+++ backend/src/models/model.js
@@ -8,6 +8,11 @@ const userSchema = new mongoose.Schema(
       trim: true,
       unique: true,
     },
+    name: {
+      type: String,
+      required: true,
+      trim
+    },
     email: {
       type: String,
       required: true,
