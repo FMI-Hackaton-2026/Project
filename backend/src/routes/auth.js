aa8a3adc16bad9742c34b03b4ac47f57475384cf
Stanislav <stanislav.igov@casovate.com>
2026-03-14 17:59:11 +0200

    refresh

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { login, me } from '../controllers/auth.controller.js';
+import { login, me, refresh } from '../controllers/auth.controller.js';
 import { requireAuth } from '../middlewares/requireAuth.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.get('/me', requireAuth ,me);
+router.post('/refresh', refresh)
 
 export default router; 
