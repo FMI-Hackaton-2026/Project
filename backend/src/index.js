0bed36aee47a029a9582c1dd35161bd12d33a804
Stanislav <stanislav.igov@casovate.com>
2026-03-14 13:44:02 +0200

    DB connection set up

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,44 @@
 import express from 'express'
+import cors from 'cors';
+import connectDB from './config/mongoDB.js';
+import 'dotenv/config';
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.use(express.json())
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000')
-})
+app.use(
+  cors({
+    origin: [
+      process.env.FRONTEND_URL,
+    ],
+    credentials: true,
+  })
+);
+
+const start = async() => {
+  try{
+    await connectDB();
+
+    const server = app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+
+    const shutdown = async () => {
+      console.log('Shutting down...');
+      server.close(() => console.log('HTTP server closed'));
+      await shutdownRedis();
+      process.exit(0);
+    };
+
+    process.on('SIGINT', shutdown);
+    process.on('SIGTERM', shutdown);
+  }
+  catch(err){
+    console.error('Startup error:', err);
+    process.exit(1);
+  }
+}  
+
+start();
